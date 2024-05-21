a0c6a8d499cff1a4846deb00a4f6676be735870c
niquiem <tokmann.nina@gmail.com>
2024-05-21 14:40:58 +0200

    Add rewards to Habit clas

--- habit.py
+++ habit.py
@@ -7,10 +7,17 @@ class Habit:
         self.streak = 0
         self.creation_date = datetime.now()
         self.completion_dates = []
+        self.reward = None
 
     def mark_complete(self):
         self.completion_dates.append(datetime.now().isoformat())
         self.streak += 1
+        if self.reward:
+            reward_message = self.reward.trigger()
+            print(f"Reward for completing {self.habit_name}: {reward_message}")
 
     def check_streak(self):
         return self.streak
+    
+    def set_reward(self, reward):
+        self.reward = reward
