1530f4ff94ec58b04cb615ba6b7a560cfbca03a6
niquiem <tokmann.nina@gmail.com>
2024-05-23 08:36:48 +0200

    Trying out marking habits incomplete

--- habit.py
+++ habit.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 
 class Habit:
-    def __init__(self, habit_name, frequency):
+    def __init__(self, habit_name, frequency, streak=0, completion_dates=None):
         self.habit_name = habit_name
         self.frequency = frequency
-        self.streak = 0
+        self.streak = streak
         self.creation_date = datetime.now()
-        self.completion_dates = []
-        self.reward = None
+        self.completion_dates = completion_dates if completion_dates else []
 
     def mark_complete(self):
         self.completion_dates.append(datetime.now().isoformat())
@@ -16,8 +15,15 @@ class Habit:
             reward_message = self.reward.trigger()
             print(f"Reward for completing {self.habit_name}: {reward_message}")
 
+    def mark_incomplete(self):
+        if self.completion_dates:
+            self.completion_dates.pop()
+            self.streak = max(0, self.streak - 1)
+        else:
+            print(f"No completions to remove for {self.habit_name}")
+
     def check_streak(self):
         return self.streak
-    
+
     def set_reward(self, reward):
         self.reward = reward
