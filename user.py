819fe050ae43ac1a29bd6c23e0019f799d4a1304
niquiem <tokmann.nina@gmail.com>
2024-05-29 20:03:23 +0200

    updated User to handle duplicate habits

--- user.py
+++ user.py
@@ -3,25 +3,38 @@ from habit import Habit
 
 class User:
     def __init__(self, username, user_id=None):
+        """
+        Initialize a new User object.
+
+        Args:
+            username (str): The username of the user.
+            user_id (int, optional): The ID of the user in the database. Defaults to None.
+
+        Attributes:
+            username (str): The username of the user.
+            user_id (int, optional): The ID of the user in the database.
+            habits (list): The list of habits associated with the user, loaded from the database.
+        """
         self.username = username
         self.user_id = user_id
         self.habits = self.load_habits()
 
-    def add_habit(self, habit): # Add a habit to the user's list of habits and save it to the database
+    def add_habit(self, habit): # Add a new habit to the user's list of habits if it doesn't already exist
+        #Raise an error if the habit already exists
+        if any(existing_habit.habit_name == habit.habit_name for existing_habit in self.habits):
+            raise ValueError(f"Habit '{habit.habit_name}' already exists.")
         self.habits.append(habit)
         add_habit_to_db(habit, self.user_id)
 
     def remove_habit(self, habit_name): # Remove a habit from the user's list of habits
         self.habits = [habit for habit in self.habits if habit.habit_name != habit_name]
 
-    def get_habits(self): # Return the user's list of habits
+    def get_habits(self): # Get the list of habits associated with the user
         return self.habits
 
-    def load_habits(self): # Load the user's habits from the database
+    def load_habits(self): # Load the habits associated with the user from the database and return them as a list of Habit objects
         habits_data = load_habits_from_db(self.user_id)
-        #Sprint(f"habits_data: {habits_data}")  # Debugging line: Uncomment to see the raw habits data from the database
         habits = []
         for habit_data in habits_data:
-            #print(f"habit_data: {habit_data}")  # Debugging line: Uncomment to see each habit's data
-            habits.append(habit_data)  # Directly append the Habit object
+            habits.append(habit_data)
         return habits
